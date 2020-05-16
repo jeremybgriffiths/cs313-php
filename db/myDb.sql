b1400110f32058870e9eec92e0f604de677fc596
jeremybgriffiths <gri18025@byui.edu>
2020-05-16 17:09:35 -0600

    Added script to create the database tables

---
+++ db/myDb.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS Checkout;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Books;
+
+CREATE TABLE Books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(256) NOT NULL,
+    author VARCHAR(256) NOT NULL,
+    genre VARCHAR(256)
+);
+
+CREATE TABLE Users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(256) NOT NULL UNIQUE,
+    userpassword VARCHAR(256) NOT NULL,
+    isadmin BOOLEAN NOT NULL
+);
+
+CREATE TABLE Checkout (
+    id SERIAL PRIMARY KEY,
+    bookid INT REFERENCES Books(id),
+    userid INT REFERENCES Users(id),
+    checkout_date DATE,
+    checkin_date DATE,
+    fine INT
+);
