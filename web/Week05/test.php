46cd67ad06a390deffbddf9a8510122b13fa2426
jeremybgriffiths <gri18025@byui.edu>
2020-05-19 15:05:37 -0600

    Connecting to db through php

--- web/Week05/test.php
+++ web/Week05/test.php
@@ -1,7 +1,12 @@
 <?php
 require_once('../../db/connectToDb.php');
 
-foreach ($db->query('SELECT username, password FROM note_user') as $row)
+
+?>
+
+
+<?php
+foreach ($dbUrl->query('SELECT username, password FROM note_user') as $row)
 {
   echo 'user: ' . $row['username'];
   echo ' password: ' . $row['password'];
