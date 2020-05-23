357f99418be3a0c0ada9bfee57cdce10675c3194
jeremybgriffiths <gri18025@byui.edu>
2020-05-23 15:27:57 -0600

    Testing

--- web/Project/user-menu.php
+++ web/Project/user-menu.php
@@ -16,7 +16,7 @@ require_once('../../db/connectToDb.php');
 <body>
     <?php
     $bookAuthor = 'Tolkien, J.R.R.';
-    $stmt = $db->prepare("SELECT title, author, genre FROM Books Where title = '$bookAuthor'");
+    $stmt = $db->prepare("SELECT title, author, genre FROM Books Where author = '$bookAuthor'");
     $stmt->execute();
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
