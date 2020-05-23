97e69aded16c38ef351173779df418eb37ea0eab
jeremybgriffiths <gri18025@byui.edu>
2020-05-23 17:48:29 -0600

    Testing

--- web/Project/display-books.php
+++ web/Project/display-books.php
@@ -17,25 +17,25 @@
     ?>
         <table>
             <tr>
-                <th> ID </th>
-                <th> Title </th>
-                <th> Author </th>
-                <th> Genre </th>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Genre</th>
             </tr>
 
             <?php foreach ($rows as $row) {
             ?>
                 <tr>
-                    <td><?php echo $row; ?> </td>
+                    <td><?php echo $row["id"]; ?> </td>
                     <td><?php echo $row["title"]; ?> </td>
                     <td><?php echo $row["author"]; ?> </td>
                     <td><?php echo $row["genre"]; ?> </td>
                 </tr>
-            <?php
+        <?php
             }
-    } else
-        echo "<center>No books found in the library by the name $search </center>";
-            ?>
+        } else
+            echo "<center>No books found in the library by the name $search </center>";
+        ?>
         </table>
 </body>
 
