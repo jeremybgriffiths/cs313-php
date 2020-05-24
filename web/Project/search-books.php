f120c6c4255729189414b62000ae0f649c46c11c
jeremybgriffiths <gri18025@byui.edu>
2020-05-23 20:39:01 -0600

    Testing

--- web/Project/search-books.php
+++ web/Project/search-books.php
@@ -1,13 +1,21 @@
 <!DOCTYPE HTML>
-<html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/search-books.css">
+</head>
 
 <body>
-    <div>
+    <div id="header">
         <h2>Simple Library Management System</h2>
     </div>
     <form action="display-books.php" method="get">
         <br>
-        <div>Enter the title of the book to be searched :
+        <div>
+            <p>Enter the title of the book to be searched: </p>
             <input type="text" name="search" size="48">
             <br></br>
             <input type="submit" value="submit">
--- web/Project/user-menu.php
+++ web/Project/user-menu.php
@@ -1,9 +1,3 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-require_once('../../db/connectToDb.php');
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,24 +5,10 @@ require_once('../../db/connectToDb.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link>
 </head>
 
 <body>
     <form action=""></form>
-    <?php
-    $bookAuthor = 'Tolkien, J.R.R.';
-    $stmt = $db->prepare("SELECT title, author, genre FROM Books Where author = '$bookAuthor'");
-    $stmt->execute();
-    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    foreach ($rows as $row) {
-        echo 'Title: ' . $row['title'];
-        echo ' author: ' . $row['author'];
-        echo ' genre: ' . $row['genre'];
-        echo '<br/>';
-    };
-    ?>
 </body>
 
 </html>
