b29967af926b3e4de52ae3fc61ddfdf70525b5d0
jeremybgriffiths <gri18025@byui.edu>
2020-05-23 23:22:56 -0600

    Testing

--- web/Project/login-form.php
+++ web/Project/login-form.php
@@ -5,17 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book Search</title>
-    <link rel="stylesheet" href="styles/">
+    <link rel="stylesheet" href="styles/login-form.css">
 </head>
 
 <body>
-
-    <!-- Make a note that the method type used is post, action page is Login.php and validate() function will get called on submit -->
     <div style="text-align:center">
         <h1>PHP Login Form using MySQL</h1>
     </div>
     <br>
-    <form name="login" method="post" action="Login.php" onsubmit="return validate();">
+    <form name="login" method="post" action="login.php" onsubmit="return validate();">
         <div>Username: <input type="text" name="username" /> </div>
         <div>Password: <input type="password" name="password" /> </div>
         <div><input type="submit" value="Login"></input> <input type="reset" value="Reset"></input></div>
