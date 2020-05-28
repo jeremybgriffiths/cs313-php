1a31c28cf538469adfcb33117183a75905ff7b44
jeremybgriffiths <gri18025@byui.edu>
2020-05-27 21:47:33 -0600

    Testing

--- web/Week06/teamActivityFunctions.php
+++ web/Week06/teamActivityFunctions.php
@@ -76,6 +76,8 @@ function insertTopic()
         die();
     }
 
+    header("Location: topicEntry.php");
+
     die(); // we always include a die after redirects. In this case, there would be no
     // harm if the user got the rest of the page, because there is nothing else
     // but in general, there could be things after here that we don't want them
@@ -127,7 +129,4 @@ function showTopics()
         echo "Error with DB. Details: $ex";
         die();
     }
-    header("Location: topicEntry.php");
-
-    die();
 }
