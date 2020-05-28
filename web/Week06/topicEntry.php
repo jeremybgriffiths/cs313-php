81e5d60146da0de41a957e382988275bd1722725
jeremybgriffiths <gri18025@byui.edu>
2020-05-27 22:21:25 -0600

    Testing

--- web/Week06/topicEntry.php
+++ web/Week06/topicEntry.php
@@ -14,14 +14,28 @@
 include("../../db/connectToDb.php");
 include("./teamActivityFunctions.php");
 
-if (isset($_POST['txtBook'])) {
-	insertTopic();
-}
+// if (isset($_POST['txtBook'])) {
+// 	insertTopic();
+// }
 ?>
 <!DOCTYPE html>
 <html>
 
 <head>
+	<script>
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				<?php
+				if (isset($_POST['txtBook'])) {
+					insertTopic();
+				}
+				?>
+			}
+		};
+		xmlhttp.open("POST", "gethint.php?q=" + str, true);
+		xmlhttp.send();
+	</script>
 	<title>Topic Entry</title>
 </head>
 
