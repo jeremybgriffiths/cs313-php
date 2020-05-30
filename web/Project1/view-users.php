a60e551363dd6b45ce39e2bc45d0eb421b0151cd
jeremybgriffiths <gri18025@byui.edu>
2020-05-30 14:29:11 -0600

    adding admin options

---
+++ web/Project1/view-users.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users</title>
+    <link rel="stylesheet" href="styles/display-books.css">
+</head>
+
+<body>
+    <h2>Users</h2>
+    <?php
+    include("../../db/connectToDb.php");
+
+    $stmt = $db->prepare("SELECT username, isadmin FROM Users ORDER BY username");
+    $stmt->execute();
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if (count($rows) > 0) {
+    ?>
+        <table>
+            <tr>
+                <th>Username</th>
+                <th>Admin</th>
+            </tr>
+
+            <?php foreach ($rows as $row) {
+            ?>
+                <tr>
+                    <td><?php echo $row["username"]; ?> </td>
+                    <td><?php echo $row["isadmin"]; ?> </td>
+                </tr>
+        <?php
+            }
+        } else
+            echo "<center>No users found</center>";
+        ?>
+        </table>
+</body>
+
+</html>
