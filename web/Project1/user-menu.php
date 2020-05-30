884a091abef1f460e1523522ee1d4124b4d507d4
jeremybgriffiths <gri18025@byui.edu>
2020-05-30 11:36:06 -0600

    Added link to add books

--- web/Project1/user-menu.php
+++ web/Project1/user-menu.php
@@ -1,24 +1,28 @@
 <html>
 <title>User Menu</title>
+
 <body>
- <?php
-     session_start();
-     $username = $_SESSION['username'];
- ?>
- 
- <div style="text-align:center"><h1>User Menu</h1></div>
- <br/>
- 
- <div style="font-weight:bold"> Welcome <?php echo $username ?> </div>
- <a href="search-books.php">Search for a Book</a>
-  
- <div style="text-align: right"><a href="logout.php">Logout</a></div>
- 
- <?php
- if(!isset($_SESSION['username']))
- {
-     header("location:login-form.php");
- }
- ?>
+    <?php
+    session_start();
+    $username = $_SESSION['username'];
+    ?>
+
+    <div style="text-align:center">
+        <h1>User Menu</h1>
+    </div>
+    <br />
+
+    <div style="font-weight:bold"> Welcome <?php echo $username ?> </div>
+    <a href="search-books.php">Search for a Book</a>
+    <a href="add-books.php"></a>
+
+    <div style="text-align: right"><a href="logout.php">Logout</a></div>
+
+    <?php
+    if (!isset($_SESSION['username'])) {
+        header("location:login-form.php");
+    }
+    ?>
 </body>
+
 </html>
