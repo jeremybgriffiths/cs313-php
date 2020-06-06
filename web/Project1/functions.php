99bd4ec9e05a2d9158ecb9bd8f77285a706d91f6
jeremybgriffiths <gri18025@byui.edu>
2020-06-06 10:41:15 -0600

    testing

--- web/Project1/functions.php
+++ web/Project1/functions.php
@@ -1,33 +1,68 @@
 <?php
-function validateInput($data) {
+function validateInput($data)
+{
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
 }
 
-function checkPassword($clientPassword) {
+function checkPassword($clientPassword)
+{
     $pattern = '/^(?=.*[[:digit:]])(?=.*[[:punct:]])(?=.*[A-Z])(?=.*[a-z])([^\s]){8,}$/';
     return preg_match($pattern, $clientPassword);
 }
 
-function registerNewUser($userName, $password) {
+function registerNewUser($userName, $password)
+{
     include("../../db/connectToDb.php");
     try {
         $sql = 'INSERT INTO Users (username, userpassword, isadmin) VALUES (:username, :userpassword, :isadmin)';
         $stmt = $db->prepare($sql);
         $stmt->bindValue(':username', $userName, PDO::PARAM_STR);
         $stmt->bindValue(':userpassword', $password, PDO::PARAM_STR);
-        $stmt->bindValue(':isadmin', false, PDO::PARAM_STR);        
-        $stmt -> execute();
+        $stmt->bindValue(':isadmin', false, PDO::PARAM_STR);
+        $stmt->execute();
 
         $rowsChanged = $stmt->rowCount();
 
         $stmt->closeCursor();
 
         return $rowsChanged;
-
     } catch (Exception $ex) {
         return "Error";
     }
+}
+
+function registerForm($actionValue, $userName = '')
+{
+    echo "<form action= ' ' method='post' class=form-horizontal>";
+
+    echo "<div class='form-inline'>";
+    echo "<label for='usernameInput' class='control-label col-sm-1'>User Name:</label>";
+    echo "<input type='text' name='userName' id='usernameInput' class='form-control' placeholder='Enter UserName' value='{$userName}'>";
+    echo "</div>";
+
+    echo "<div class='form-inline'>";
+    echo "<label for='passwordInput' class='control-label col-sm-1'>Password:</label>";
+    echo "<input type='password' name='password' id='passwordInput' class='form-control' placeholder='Enter password' pattern='(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}'><span id='errorThing' class='hidden'>*</span>";
+
+    echo "<div>";
+    echo "<small id='passwordInputHelp' class='form-text text-muted'>Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters</small>";
+    echo "</div>";
+    echo "</div>";
+
+    echo "<div class='form-inline'>";
+    echo "<label for='passwordConfirmationInput' class='control-label col-sm-1'>Password:</label>";
+    echo "<input type='password' name='passwordConfirmation' id='passwordConfirmationInput' class='form-control' placeholder='Enter password' onkeyup='checkPassword()'>";
+
+    echo "<div>";
+    echo "<small id='passwordInputHelp' class='form-text text-muted'>Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters</small>";
+    echo "</div>";
+    echo "</div>";
+
+    echo "<input type='hidden' name='action' class='form-control' value='{$actionValue}' onchange='checkPassword()>";
+    echo "<span id='passwordCheck'></span>";
+    echo "<input type='submit' class='btn btn-primary' value='Register'>";
+    echo "</form>";
 }
--- web/Project1/register.php
+++ web/Project1/register.php
@@ -15,10 +15,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
         $rowsChanged = registerNewUser($username, $hashedPassword);
-
-        // echo $rowsChanged;
-
-        // If successfully inserted the user info, redirect them to the login page
         if ($rowsChanged == 1) {
             header('Location: login-form.php?username=' . $username . "&registrationSuccessfull=true");
             die();
@@ -35,14 +31,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <head>
     <meta charset="utf-8">
-    <meta name="description" content="W07 Team Assignment">
-    <meta name="author" content="Scott Mosher">
-    <meta content="width=device-width, initial-scale=1" name="viewport" />
-
-    <!-- Need to include bootstrap -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-
-    <title>W07 Team Assignment</title>
+    <link rel="stylesheet" href="styles/login-form.css">
+    <title>Register User</title>
 
     <script src="javascript.js"></script>
 </head>
@@ -51,28 +43,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <header>
     </header>
     <main>
-        <!-- ********** NOTE Both signin forms have the same field IDs, so if both are turned on there might be issues **********  -->
         <section>
             <h2>Please Register</h2>
-
             <?php
-            // Display the form with just a single password box
-            // signUpForm('registrationRequest'); 
-            ?>
-        </section>
-        <hr>
-        <section>
-            <h2>Please Register with Password Confirmation</h2>
-            <p>This will use a 2nd password input box to make sure the user entered in the password they want to use. It will use an AJAX function to check the passwords to make sure they match</p>
-            <?php
-
             echo $message;
-            // Display the form
-            signUpFormDoublePassword('registrationRequest');
+            registerForm('registrationRequest');
             ?>
         </section>
-
-
     </main>
     <footer>
     </footer>
