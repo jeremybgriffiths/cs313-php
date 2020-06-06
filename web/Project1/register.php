cbb61c75f5958aa97f1f445a098b6138b9d79310
jeremybgriffiths <gri18025@byui.edu>
2020-06-06 11:38:32 -0600

    testing

--- web/Project1/register.php
+++ web/Project1/register.php
@@ -41,6 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </div>
     <br>
     <?php
+    include('functions.php');
     echo $message;
     echo "<form action= ' ' method='post' class=form-horizontal>";
 
