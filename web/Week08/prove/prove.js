88afe5dabefd997e525821c4272c6f6708d3313c
jeremybgriffiths <gri18025@byui.edu>
2020-06-13 15:27:25 -0600

    Finished week 8 prove assignment

--- web/Week08/prove/prove.js
+++ web/Week08/prove/prove.js
@@ -1,16 +1,19 @@
 const http = require('http');
+const https = require('https');
+const url = require('url');
 
 const server = http.createServer(onRequest);
 server.listen(8888);
 
 function onRequest(req, res) {
-    if (req.url === '/home') {
+    const query = url.parse(req.url, true);
+    if (query.pathname === '/home') {
         res.writeHead(200, {
             "Content-Type": "text/html",
         });
         res.write('<h1>Welcome to the Home Page</h1>');
         res.end();
-    } else if (req.url === '/getData') {
+    } else if (query.pathname === '/getData') {
         res.writeHead(200, {
             "Content-Type": "application/json"
         });
@@ -19,6 +22,27 @@ function onRequest(req, res) {
             'class': 'cse341'
         }));
         res.end();
+    } else if (query.pathname === '/getApiData') {
+        const request = require('request');
+
+        request('https://lozapi.glitch.me/games/', {
+            json: true
+        }, (error, response, body) => {
+            if (error) {
+                return console.log(err);
+            }
+
+            res.writeHead(200, {
+                "Content-Type": "text/html",
+            });
+
+            res.write('<h1>Legend of Zelda Game titles</h1>');
+
+            for (i in body.results) {
+                res.write('<h3>' + body.results[i].title + '</h3>');
+            }
+            res.end();
+        });
     } else {
         res.writeHead(404, {
             "Content-Type": "text/html"
